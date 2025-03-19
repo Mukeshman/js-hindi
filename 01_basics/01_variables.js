b2c99ced6e7597d383570c8e91e4bdced258ce6b
Mukeshman <109132636+Mukeshman@users.noreply.github.com>
2025-03-19 16:28:19 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,13 @@
+const accountId = 14423
+let accountEmail = "mukeshpradhan@gmail.com"
+var accountPassword = "12345"
+accountCity = "Bhubaneswar"
+let accountState;
+
+accountEmail = "mk@gmail.com"
+accountPassword = "123"
+accountCity ="Berhampur"
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
+
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,19 @@
+"use strict";
+
+console.log(3+3)
+
+let name="Mukesh"
+let age = 30
+let isLoggedIn = false
+let state
+
+// number
+//bigint
+//string
+//boolean
+//null is a object
+//undefined is undefined
+//symbol is unique
+
+console.log(typeof null)
+console.log(typeof undefined);
